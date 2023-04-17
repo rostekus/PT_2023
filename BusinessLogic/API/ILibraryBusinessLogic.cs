fc239bcc3c8e9f7331cf1107d747ad98f6c4c42a
Wiktoria Śliwińska <242231@edu.p.lodz.pl>
2023-04-17 13:54:41 +0200

    feat: business logic interaface

--- BusinessLogic/API/ILibraryBusinessLogic.cs
+++ BusinessLogic/API/ILibraryBusinessLogic.cs
@@ -2,5 +2,6 @@ namespace BusinessLogic.API;
 
 public interface ILibraryBusinessLogic
 {
-    
+    void BorrowBook(string userId, string stateId);
+    void ReturnBook(string userId,string stateId);
 }
