c93677a0cdc69933821e1c309280db231ac56ad4
Wiktoria Śliwińska <242231@edu.p.lodz.pl>
2023-04-17 13:56:30 +0200

    feat: added IRepository methods

--- DataAccess/API/IRepository.cs
+++ DataAccess/API/IRepository.cs
@@ -1,6 +1,10 @@
 namespace DataAccess.API;
 
-public interface IRepository
+public interface IRepository<T>
 {
-    
+    List<T> GetAll();
+    T GetById(string id);
+    void Add(T entity);
+    void Update(T entity);
+    void Delete(T entity);
 }
