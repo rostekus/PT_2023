24e37d2b369d8a2fdc3edc94bcafd17c7741d232
Wiktoria Śliwińska <242231@edu.p.lodz.pl>
2023-04-17 13:55:21 +0200

    feat: return implemantation

--- BusinessLogic/SampleImplementation/Return.cs
+++ BusinessLogic/SampleImplementation/Return.cs
@@ -1,6 +1,20 @@
+using DataAccess.API;
+
 namespace BusinessLogic.SampleImplementation;
 
-public class Return
+public class Return : IReturn
 {
-    
+    public string Guid { get; }
+    public DateTime Time { get; }
+
+    public IState State { get; }
+    public IClient Client { get; }
+
+    public Return(string guid, DateTime time, IState state, IClient client)
+    {
+        Guid = guid;
+        Time = time;
+        State = state;
+        Client = client;
+    }
 }
