4c5c55e735886d34fd833bc621c1357e71a9123c
Rostyslav Mosorov <34031791+rostekus@users.noreply.github.com>
2023-05-08 16:28:05 +0200

    fix: changed name to Books

--- PT/DataAccess/API/ILibraryDataContext.cs
+++ PT/DataAccess/API/ILibraryDataContext.cs
@@ -3,7 +3,7 @@ namespace DataAccess.API;
 public interface ILibraryDataContext
 {
     public List<IClient> Clients { get; set; }
-    public Dictionary<string, IBook> catalog { get; set; }
+    public Dictionary<string, IBook> Books { get; set; } 
     public List<ILibraryEvent> Events { get; set; }
     public List<IState> States { get; }
     
