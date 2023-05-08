ad1ced47d0c8e6e86a7b99c73f7e59d4d8ef3afb
Rostyslav Mosorov <34031791+rostekus@users.noreply.github.com>
2023-05-08 16:29:05 +0200

    feat: updated book repository to use dict

--- PT/DataAccess/SampleImplementation/BookRepository.cs
+++ PT/DataAccess/SampleImplementation/BookRepository.cs
@@ -2,7 +2,7 @@ using DataAccess.API;
 
 namespace DataAccess.SampleImplementation;
 
-public class BookRepository : IBookRepository
+internal class BookRepository : IBookRepository
 {
     private readonly LibraryDataContext _context;
     public BookRepository(LibraryDataContext context)
@@ -10,61 +10,35 @@ public class BookRepository : IBookRepository
         _context = context;
     }
 
-    public List<IBook> GetAll() => _context.Books;
+    public IEnumerable<IBook> GetAll() => _context.Books.Values;
     
     public IBook GetById(string guid)
     {
-        // check if user exists with the same guid in repository
-        var book = this._context.Books.Find(checkBook => checkBook.Guid == guid);
-        if (book== null)
-        {
-            throw new Exception("No Book with specified GUID");
-        }
-
+        var book = this._context.Books[guid]
         return book;
     }
 
     public void Add(IBook entity)
     {
-        this._context.Books.Add(entity);
+        this._context.Books.Add(entity.Guid, entity);
     }
 
     public void Update(IBook entity)
     {
-        for (var index = 0; index < _context.Books.Count; index++)
-        {
-            var user = _context.Books[index];
-            if (user.Guid != entity.Guid)
-                continue;
-
-            _context.Books.Remove(user);
-            _context.Books.Add(entity);
-            return;
-        }
+      this._context.Books[entity.Guid] = entity;
     }
 
 
     public void Create(IBook book)
     {
-        // check if book exists with the same guid in repository
-        var bookDuplicate = this._context.Books.Find(checkBook => checkBook.Guid == book.Guid);
-        if (bookDuplicate == null)
-        {
-            throw new Exception("Book with that GUID exists in repository");
-        }
-        _context.Books.Add(book);
+       this._context.Books.Add(entity.Guid, entity); 
     }
 
     public void Delete(IBook book)
     {
-        var foundBook = this._context.Books.Find(checkBook => checkBook.Guid == book.Guid);
-        if (book== null)
-        {
-            throw new Exception("No Book with specified GUID");
-        }
-
-        this._context.Books.RemoveAll(checkBook => checkBook.Guid == book.Guid);
-
+        foreach (var s in this._context.Books)
+            if (s.Key == book.Guid)
+                this._context.Books.Remove(s.Key);
     }
 
 
