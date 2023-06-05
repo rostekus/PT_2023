1c7718073abce45e9a322413821a867d5b280199
Rostyslav Mosorov <34031791+rostekus@users.noreply.github.com>
2023-06-05 22:10:06 +0200

    feat: added more crud

---
+++ PT/Service/API/IEventCRUD.cs
@@ -0,0 +1,24 @@
+﻿using DataLayer.API;
+using Service.Implementation;
+
+namespace Service.API;
+
+public interface IEventCRUD
+{
+    static IEventCRUD CreateEventCRUD(IDataRepository? dataRepository = null)
+    {
+        return new EventCRUD(dataRepository ?? IDataRepository.CreateDatabase(ConnectionString.GetConnectionString()));
+    }
+
+    Task AddEvent(int id, int stateId, int userId, string type);
+
+    Task<IEventDTO> GetEvent(int id);
+
+    Task UpdateEvent(int id, int stateId, int userId, string type);
+
+    Task DeleteEvent(int id);
+
+    Task<Dictionary<int, IEventDTO>> GetAllEvents();
+
+    Task<int> GetEventsCount();
+}
---
+++ PT/Service/API/IProductCRUD.cs
@@ -0,0 +1,24 @@
+﻿using DataLayer.API;
+using Service.Implementation;
+
+namespace Service.API;
+
+public interface IProductCRUD
+{
+    static IProductCRUD CreateProductCRUD(IDataRepository? dataRepository = null)
+    {
+        return new ProductCRUD(dataRepository ?? IDataRepository.CreateDatabase(ConnectionString.GetConnectionString()));
+    }
+
+    Task AddProduct(int id, string productName, string productDescription, float price);
+
+    Task<IProductDTO> GetProduct(int id);
+
+    Task UpdateProduct(int id, string productName, string productDescription, float price);
+
+    Task DeleteProduct(int id);
+
+    Task<Dictionary<int, IProductDTO>> GetAllProducts();
+
+    Task<int> GetProductsCount();
+}
