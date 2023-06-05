a4b479bdc4bd96e9b4df30fa5f0f9ace6667bc35
Rostyslav Mosorov <34031791+rostekus@users.noreply.github.com>
2023-06-05 22:09:52 +0200

    feat: mock crud dto for tests

---
+++ PT/ServiceTest/MockItems/MockStateDTO.cs
@@ -0,0 +1,19 @@
+﻿using Service.API;
+
+namespace ServiceTest.MockItems
+{
+    internal class MockStateDTO : IStateDTO
+    {
+        public int ProductId { get; set; }
+        public int StateId { get; set; }
+
+        public bool Available { get; set; }
+
+        public MockStateDTO(int id, int productId, bool available)
+        {
+            this.StateId = id;
+            this.ProductId = productId;
+            this.Available = available;
+        }
+    }
+}
